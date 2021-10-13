3d4d41e080616bf13547d50da534859d5e65093d
AniRed15 <77673895+AniRed15@users.noreply.github.com>
2021-10-13 17:01:16 +0530

    Add files via upload

---
+++ spring-boot-jpa-testing/spring-boot-jpa-testing/src/main/java/empmng/springbootjpatesting/Employee.java
@@ -0,0 +1,34 @@
+package empmng.springbootjpatesting;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+	
+	@Column(name = "last_name", nullable = false)
+	private String lastName;
+	
+	@Column(name = "email", nullable = false)
+	private String email;
+}
